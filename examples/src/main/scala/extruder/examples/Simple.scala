64ff53c868edb5695f3132feca5f9ad9fab9ffaf
janstenpickle <jansen.chris@gmail.com>
2017-06-27 21:37:23 +0100

    Update simple example

--- examples/src/main/scala/extruder/examples/Simple.scala
+++ examples/src/main/scala/extruder/examples/Simple.scala
@@ -2,44 +2,35 @@ package extruder.examples
 
 import java.net.URL
 
-import cats.syntax.either._
-import cats.syntax.validated._
-import com.typesafe.config.{ConfigFactory, ConfigObject}
-import eu.timepit.refined.api.Refined
-import eu.timepit.refined.numeric._
-import extruder.core._
-import extruder.typesafe.TypesafeConfig
+import extruder.core.MapConfig._
+import extruder.core.{EitherErrors, EitherThrowable, ValidationErrors}
+import extruder.monix._
+import monix.eval.Task
+import monix.execution.Scheduler.Implicits.global
 
-import scala.collection.JavaConverters._
+import scala.concurrent.Future
 import scala.concurrent.duration.{Duration, FiniteDuration}
-import extruder.refined._
-import extruder.system.{EnvironmentConfig, SystemPropertiesConfig}
 
-case class CC(a: String = "test", b: String = "test2", c: Int, d: Option[CC2], e: CC3, f: Set[Int], dur: Duration, finDur: FiniteDuration)
-case class CC2(x: String = "test4", y: Option[Int] = Some(232), z: CC3)
+case class CC(
+  a: String = "test",
+  b: String = "test2",
+  c: Int,
+  d: CC2,
+  e: CC3,
+  f: Set[Int],
+  dur: Duration,
+  finDur: FiniteDuration
+)
+case class CC2(x: String = "test4", y: Option[Int] = Some(232), z: CC3, dfs: Long)
 case class CC3(a: Option[String])
 case class CC4(a: Option[CC3])
 
-
-case class Testing(s: Option[Long], d: Set[String], i: Int = 1)
-
 sealed trait Sealed
 case object ObjImpl extends Sealed
-case class CCImpl2(a: String) extends Sealed
 case class CCImpl(a: String, i: Long = 76, u: URL, s: Set[Int], cc: Option[CC4]) extends Sealed
 
-case class Testing2(a: Set[Int] = Set(1,2), df: Option[Long] = Some(32L), dsf: Option[List[String]] = Some(List("dfs", "sdfsdf")))
-case class Testing3(b: String, c: Testing2)
-
-case class Hello(s: ConfigObject = ConfigFactory.parseMap(Map("sdfsdf" -> "dsfsdf").asJava).root())
-
-trait Thing[T] {
-  def t: T
-}
-case class ThingImpl(t: String) extends Thing[String]
-
-
 object Simple extends App {
+
   val config = Map(
     "cc.c" -> "2000",
     "cc.a" -> "sdfsf",
@@ -48,38 +39,16 @@ object Simple extends App {
     "cc3.a" -> "hello",
     "cc.f" -> "2, 3",
     "cc.dur" -> "Inf",
-    "cc.findur" -> "22 days"
+    "cc.findur" -> "22 days",
+    "ccimpl2.a" -> "sdfds",
+    "ccimpl2.b" -> "34",
+    "thingimpl.t" -> "sadfsf"
   )
 
-  println(MapConfig.decode[CC](config))
-
-  val sealedObjResolvers = Map("type" -> "ObjImpl")
-
-  println(MapConfig.decode[Sealed](sealedObjResolvers).map(MapConfig.encode[Sealed]))
-
-  println(MapConfig.encode[Sealed](ObjImpl))
-
-
-  println(MapConfig.decode[CC](config).map(MapConfig.encode[CC]))
-
-  println(MapConfig.decode[Sealed](sealedObjResolvers))
-
-
-  println(SystemPropertiesConfig.decode[CC])
-
-  println(TypesafeConfig.decode[CC](ConfigFactory.parseMap(config.asJava)))
-
-  println(MapConfig.decode[Int Refined Positive](Map("" -> "23")).map(MapConfig.encode[Int Refined Positive]))
-
-  println(EnvironmentConfig.decode[String](Seq("home")))
-
-
-  println(MapConfig.parameters[Sealed])
-  
-  println(EnvironmentConfig.parameters[Sealed])
-
-  println(TypesafeConfig.parameters[Sealed])
-
+  println(decode[CC](config))
+  println(decode[CC, Future, Throwable](config))
+  println(decodeIO[CC](config))
+  println(decodeIO[CC, EitherThrowable, Throwable](config))
+  println(decodeAsync[CC, Task](config))
+  println(decodeAsync[CC, Task, EitherErrors, ValidationErrors](config))
 }
-
-
